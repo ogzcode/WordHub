f4df48db9a92fe237662e2b8ef2736ed678ff65f
ogzCode <oguzguc44@gmail.com>
2023-01-22 14:22:46 +0300

    Application codes are organized and components are defined

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import data from "./name.json";
+
+function NavBar() {
+  return (
+    <nav className='nav'>
+      <a href="#">Search</a>
+      <a href='#'>Saved</a>
+    </nav>
+  );
+}
+
+function SearchBar(props) {
+  const [value, setValue] = useState("");
+
+  const handleValueChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (value !== "") {
+      props.onClickForGetValue(value);
+      setValue("");
+    }
+  };
+
+  return (
+    <div className='search__bar'>
+      <input type="text" value={value} onChange={(e) => handleValueChange(e)} placeholder="Enter word..."/>
+      <button onClick={() => handleSubmit()}>Search</button>
+    </div>
+  );
+}
+
+function Word() {
+  return (
+    <div className='word__box'>
+      <div className='word'>
+        <div className='header'>
+          <p className='word__text'>hello</p>
+          <p className='saved'></p>
+        </div>
+        <div className='type__box'>
+          <span className='type'>noun</span>
+          <span className='phonetic'>/{"/həˈləʊ/"}/</span>
+        </div>
+        <p className='definitions'>{"\"Hello!\" or an equivalent greeting."}</p>
+        <p className='example'>{"Hello, everyone."}</p>
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  
+  const handleSearch = (value) => {
+    console.log(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <div className='search__container'>
+        <SearchBar onClickForGetValue={handleSearch}/>
+      </div>
+      <Word />
     </div>
   );
 }
