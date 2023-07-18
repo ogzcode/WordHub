9b98a3339cdce4d6d353304ff99c2d3be5d424d7
Oğuzhan Güç <oguzhan.guc@ayssoft.com>
2023-07-18 15:49:53 +0300

    Added Tailwind and added other tools

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import router from './router'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(router)
+app.use(pinia)
+
+app.mount('#app')
